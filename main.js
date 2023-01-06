702c69c24a77083f349d5cd73f36dffde9452623
Paper-Folding <specialwave@foxmail.com>
2023-01-06 09:39:11 +0800

    fixed some style

--- main.js
+++ main.js
@@ -8,17 +8,20 @@ import "bootstrap-icons/font/bootstrap-icons.css";
  * @param preset color preset to show
  * @param { iconClassName, background, border, fontColor } style configurations that will be overwrite(if passed) with preset
  */
-function showToast(text = "", duration = 3000, preset = null, { iconClassName = undefined, background = undefined, border = undefined, fontColor = undefined }) {
-    let styleToApply = {
-        iconClassName,
-        background,
-        border,
-        fontColor
-    };
+function showToast(text = "", duration = 3000, preset = null, { iconClassName = undefined, background = undefined, border = undefined, fontColor = undefined } = {}) {
+    let styleToApply = { iconClassName, background, border, fontColor };
     Object.keys(styleToApply).forEach(key => styleToApply[key] === undefined && delete styleToApply[key]);  // remove undefined style
     if (preset) {
         let presetStyle = {};
         switch (preset) {
+            case 'primary':
+                presetStyle = {
+                    iconClassName: 'bi-info-lg',
+                    background: '#cfe2ff',
+                    border: '1px solid #9ec5fe',
+                    fontColor: '#0a58ca'
+                }
+                break;
             case 'success':
                 presetStyle = {
                     iconClassName: 'bi-check-lg',
@@ -28,8 +31,20 @@ function showToast(text = "", duration = 3000, preset = null, { iconClassName = 
                 }
                 break;
             case 'warning':
+                presetStyle = {
+                    iconClassName: 'bi-exclamation-lg',
+                    background: '#ffe69c',
+                    border: '1px solid #ffd746',
+                    fontColor: '#997404'
+                }
                 break;
             case 'danger':
+                presetStyle = {
+                    iconClassName: 'bi-x-lg',
+                    background: '#f8d7da',
+                    border: '1px solid #f1aeb5',
+                    fontColor: '#b02a37'
+                }
                 break;
         }
         styleToApply = { ...presetStyle, ...styleToApply };
@@ -47,7 +62,7 @@ function showToast(text = "", duration = 3000, preset = null, { iconClassName = 
             fontSize: '1.5em',
             borderRadius: '6px',
             border: styleToApply.border,
-            boxShadow: '3px -3px 8px 1px rgba(0,0,0,0.20)',
+            boxShadow: 'rgb(0 0 0 / 10%) 2px -2px 5px 2px',
         },
         avatar: styleToApply.iconClassName,
         offset: {
@@ -57,5 +72,11 @@ function showToast(text = "", duration = 3000, preset = null, { iconClassName = 
     }).showToast();
 }
 
-showToast("Hello World!", 10000, 'success');
+showToast("I just want to notify you.", -1, 'primary');
+
+showToast("Hello World!", -1, 'success');
+showToast("Hello World!", -1, 'success', { iconClassName: 'bi-heart' });
+
+showToast("Ow man, what are you doing?", -1, 'warning');
+showToast("Get out of there quickly!", -1, 'danger');
 
