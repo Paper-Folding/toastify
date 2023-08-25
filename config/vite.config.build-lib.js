b5dfd2f414a7c35a829255e1b43d1ec4629f68cc
chenjianbo <specialwave@foxmail.com>
2023-08-25 19:44:03 +0800

    - prepare github pages - update vite config

---
+++ config/vite.config.build-lib.js
@@ -0,0 +1,44 @@
+import { resolve } from 'path';
+import { defineConfig } from "vite";
+
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: resolve(__dirname, '../lib/toastify-es.js'),
+            name: 'Toastify',
+            fileName: 'toastify-es',
+            formats: ['umd'],
+        },
+        outDir: "dist",
+        rollupOptions: {
+            output: {
+                assetFileNames: (chunkInfo) => {
+                    if (chunkInfo.name === 'style.css')
+                        return 'toastify.min.css';
+                },
+                entryFileNames: 'toastify.min.js',
+            },
+            plugins: [
+                {
+                    name: 'banner',
+                    enforce: 'post',
+                    generateBundle(options, bundle) {
+                        const banner = `/*! Toastify js ${process.env.npm_package_version}
+ * https://github.com/Paper-Folding/toastify
+ * @license MIT licensed
+ * 
+ * Copyright (C) 2018 Varun A P
+ */
+`;
+                        for (const module of Object.values(bundle)) {
+                            if (module.type === 'chunk') {
+                                module.code = banner + module.code;
+                            }
+                        }
+                    }
+                }
+            ]
+        }
+    }
+})
---
+++ config/vite.config.dev.js
@@ -0,0 +1,9 @@
+import { resolve } from 'path';
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    server: {
+        host: true,
+        port: 3000
+    },
+})
